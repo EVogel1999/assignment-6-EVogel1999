6f62561e308873ac941f1b5d875527f376d94bc0
EVogel1999 <eav990@gmail.com>
2019-03-30 14:54:25 -0400

    Add db connection in datastore

---
+++ src/datastore.ts
@@ -0,0 +1,25 @@
+
+import { Collection, MongoClient, ObjectId } from 'mongodb';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const MONGO_CONNECT = process.env.MONGO_CONNECT || '';
+
+export class DataStore {
+    tasks: Collection;
+
+    constructor(client: MongoClient) {
+        this.tasks = client.db().collection('tasks');
+    }
+
+    static async connect() {
+        return new Promise<MongoClient>((resolve, reject) =>
+          MongoClient.connect(MONGO_CONNECT,
+            async (e: Error, client: MongoClient) => {
+                if (e)
+                    reject(e);
+                resolve(client);
+        }));
+    }
+
+}
